452dd2ccd3c46617b40d242100cdec54046513cc
Miguel Alcantara <miguel.alc@gmail.com>
2020-11-29 12:15:57 -0500

    first commit

---
+++ src/main/java/pe/abovo/simple_sample/simplesample/DemoApplication.java
@@ -0,0 +1,13 @@
+package pe.abovo.simple_sample.simplesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
